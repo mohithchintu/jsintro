f9dbe619349bb78be6672f587145e72145bb660b
Mohith <konireddynagamohithreddy@gmail.com>
2022-12-06 14:15:39 +0000

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,31 @@
+// console.log("hehehe");
+// console.log("start");
+// const cbit = {
+//     cse : {
+//         cse_1 : "Core",
+//         cse_2 : "Core",
+//         cse_3 : "Core",
+//         cse_4 : "Machine Learning",
+//         cse_5 : "IOT, Cyber Security with Blockchain technology",
+//     },
+//     EEE : "IDK",
+//     ECE : "IDK",
+//     IT : "IDK",
+// };
+// console.log(cbit);
+// console.log(cbit.cse.cse_5);
+
+
+// function factorial(n){
+//     if(n == 1){
+//         return 1;
+//     } else{
+//         return n*factorial(n-1);
+//     }
+// }
+// console.log(factorial(5));
+// console.log(factorial(10));
+// console.log(factorial(1));
+
+
+
